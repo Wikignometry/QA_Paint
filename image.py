04ddc9a70c2aa9b31f0f558467320b79598f5fe2
haok1402 <haok1402@gmail.com>
2021-11-06 17:26:06 -0400

    :sparkles: displayImage

--- image.py
+++ image.py
@@ -14,17 +14,25 @@ class AppImage:
     def exportImage(self, path: str) -> None:
         self.initData.save(path)
 
+    def displayImage(self, app: TopLevelApp, canvas: WrappedCanvas) -> None:
+        if self.initData:
+            self.tempData = ImageTk.PhotoImage(self.initData)
+            canvas.create_image(app.width / 2, app.height / 2, image=self.tempData)
+
+    def cropImage(self, x0: int, y0: int, x1: int, y1: int) -> None:
+        self.initData = self.initData.crop((x0, y0, x1, y1))
+
 
 def appStarted(app):
     app.image = AppImage()
     # testing purposes
-    app.image.importImage(path="test/importImage.jpg")
+    app.image.importImage(path="./test/importImage.jpg")
+    app.image.cropImage(0, 0, 250, 250)
     app.image.exportImage(path="./test/exportImage.jpg")
 
 
 def redrawAll(app, canvas):
-    if app.image.tempData:
-        canvas.create_image(app.width / 2, app.height / 2, image=app.image.tempData)
+    app.image.displayImage(app, canvas)
 
 
 runApp(width=500, height=500)
