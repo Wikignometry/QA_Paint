dcd2445d6580a0fbadcc4cca990d5985ea2b015d
Ben Nguyen <hnguyent@andrew.cmu.edu>
2021-11-06 19:56:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,59 +4,48 @@ from polygon import *
 from line import *
 from oval import *
 
-
 def appStarted(app):
-    app.polygon = Polygon()
-    app.line = Line()
     app.status = 'Polygon'
-    app.actions = [[]]
+    app.objects = []
 
 def keyPressed(app, event):
-    if event.key == 'p':
-
+    if event.key == 'l':
         app.status = 'Line'
-    elif event.key == 'g':
+    elif event.key == 'p':
         app.status = 'Polygon'
+    elif event.key == 'o':
+        app.status = 'Oval'
+    
     elif event.key == 'w':
-        print(app.actions)
+        print(app.objects)
+        
     elif event.key == 'z':
         undo(app)
-    pass
-
-def undo(app):
-    #Edge case of nothing
-    if app.actions == [[]]:
-        return
-
-    action = app.actions[-1]
-    if action[0] == 'Line':
-        app.line.undo()
-    elif action[0] == 'Polygon':
-        app.polygon.undo()
-    app.actions.pop()
-
 
 def mousePressed(app, event):
-    #Adds an action to the history (WORKS FOR 2 CLICKS)
-    if len(app.actions[-1])==2:
-        app.actions[-1].append((event.x, event.y))
-    else:
-        app.actions.append([app.status, (event.x, event.y)])
+    if app.status == 'Line':
+        app.objects.append(Line(event.x, event.y))
+    elif app.status == 'Polygon':
+        app.objects.append(Polygon(event.x, event.y))
+    elif app.status == 'Oval':
+        app.objects.append(Oval(event.x, event.y))
 
-    #Makes objects
-    if app.status == 'Polygon':
-        app.polygon.assignPoint(event.x, event.y)
-    elif app.status == 'Line':
-        app.line.assignPoint(event.x, event.y)
+def mouseDragged(app, event):
+    app.objects[-1].currentLocation = (event.x, event.y)
 
-def mouseMoved(app, event):
-    if app.status == 'Polygon':
-        app.polygon.currentLocation = (event.x,event.y)
-    elif app.status == 'Line':
-        app.line.currentLocation=(event.x,event.y)
+
+def mouseReleased(app, event):
+    if len(app.objects) > 0:
+        app.objects[-1].assignPoints(event.x, event.y)
+
+def undo(app):
+    if app.objects == []:
+        return
+    app.objects.pop()
+
 
 def redrawAll(app, canvas):
-    app.polygon.draw(canvas)
-    app.line.draw(canvas)
+    for object in app.objects:
+        object.draw(canvas)
 
 runApp(width=500,height=500)
