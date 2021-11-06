dc1284c8eba43d8f18c0accfc04c1d8a5552bb98
Ben Nguyen <hnguyent@andrew.cmu.edu>
2021-11-06 18:04:51 -0400

    Create line.py

---
+++ line.py
@@ -0,0 +1,25 @@
+class Line():
+    def __init__(self):
+        self.points = []
+
+    def assignPoint(self, x, y):
+        self.points.append((x, y))
+
+    def draw(self, canvas):
+        for i in range(0, len(self.points)-1,2):
+            x0,y0=self.points[i]
+            x1,y1=self.points[i+1]
+            # if math.isclose(x0,x1,abs_tol=1)
+            canvas.create_line(x0, y0, x1, y1, width=5)
+        if len(self.points)%2==1:
+            x0,y0=self.points[-1]
+            x1,y1=self.currentLocation
+            canvas.create_line(x0, y0, x1, y1, width=5)
+
+    def undo(self):
+        if len(self.points) > 0:
+            if len(self.points)%2==0:
+                self.points.pop()
+                self.points.pop()
+            else:
+                self.points.pop()
