8eefe41f150c7698e64b4d0397110a9e279e0383
haok1402 <haok1402@gmail.com>
2021-11-06 17:14:27 -0400

    :sparkles: exportImage

--- image.py
+++ image.py
@@ -1,21 +1,30 @@
 from cmu_112_graphics import *
+from PIL import Image
 
 
-class Image:
+class AppImage:
     def __init__(self):
-        self.data = None
+        self.initData = None
+        self.tempData = None
 
-    def importImage(self, app: TopLevelApp, path: str) -> None:
-        self.data = ImageTk.PhotoImage(app.loadImage(path))
+    def importImage(self, path: str) -> None:
+        self.initData = Image.open(path)
+        self.tempData = ImageTk.PhotoImage(self.initData)
+
+    def exportImage(self, path: str) -> None:
+        self.initData.save(path)
 
 
 def appStarted(app):
-    app.image = Image()
-    app.image.importImage(app, path="./test/pikachu.jpg")
+    app.image = AppImage()
+    # testing purposes
+    app.image.importImage(path="test/importImage.jpg")
+    app.image.exportImage(path="./test/exportImage.jpg")
 
 
 def redrawAll(app, canvas):
-    if app.image.data: canvas.create_image(app.width / 2, app.height / 2, image=app.image.data)
+    if app.image.tempData:
+        canvas.create_image(app.width / 2, app.height / 2, image=app.image.tempData)
 
 
 runApp(width=500, height=500)
