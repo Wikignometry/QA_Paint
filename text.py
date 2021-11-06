c7e5e511a264340e195c6255973ff546bd72a235
Wikignometry <pphanach@andrew.cmu.edu>
2021-11-06 16:23:28 -0400

    create helpers.py and text.py

---
+++ text.py
@@ -0,0 +1,13 @@
+from tools import *
+
+
+class Text():
+
+    def __init__(self, icon, name, location):
+        super().__init__(icon, name, location)
+        self.color = None
+        self.font = None
+        self.size = None
+        
+
+
