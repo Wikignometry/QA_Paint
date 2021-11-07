f1575e3b9d815f05559ac6ebff9bb5590dd2e93a
Ben Nguyen <hnguyent@andrew.cmu.edu>
2021-11-06 20:17:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from cmu_112_graphics import *
 from polygon import *
 from line import *
 from oval import *
+from image import *
 
 def appStarted(app):
-    app.status = 'Polygon'
+    app.status = 'Crop'
+    app.image = AppImage()
     app.objects = []
 
 def keyPressed(app, event):
@@ -15,7 +17,19 @@ def keyPressed(app, event):
         app.status = 'Polygon'
     elif event.key == 'o':
         app.status = 'Oval'
+    elif event.key == 'c':
+        app.status = 'Crop'
+            
+    # https://pythonspot.com/tk-file-dialogs/
+    if event.key == "control-o":
+        filePath = filedialog.askopenfilename(initialfile="import-image", defaultextension=".jpg", )
+        if filePath: app.image.importImage(path=filePath)
+    if event.key == "control-s" and app.image.tempData:
+        filePath = filedialog.asksaveasfilename(initialfile="export-image", defaultextension=".jpg",
+                                                filetypes=[("ImageFile", ".jpg")])
+        if filePath: app.image.exportImage(path=filePath)
     
+
     elif event.key == 'w':
         print(app.objects)
         
@@ -29,14 +43,28 @@ def mousePressed(app, event):
         app.objects.append(Polygon(event.x, event.y))
     elif app.status == 'Oval':
         app.objects.append(Oval(event.x, event.y))
+    elif app.status == 'Crop':
+        if app.image.action["crop"]["ing"]:
+            app.image.action["crop"]["sPos"] = (event.x, event.y)
 
 def mouseDragged(app, event):
-    app.objects[-1].currentLocation = (event.x, event.y)
+    if app.status == 'Line' or app.status == 'Polygon' or app.status == 'Oval':
+        app.objects[-1].currentLocation = (event.x, event.y)
+    elif app.status == 'Crop':
+        if app.image.action["crop"]["ing"]:
+            app.image.action["crop"]["dPos"] = (event.x, event.y)       
+
 
 
 def mouseReleased(app, event):
-    if len(app.objects) > 0:
-        app.objects[-1].assignPoints(event.x, event.y)
+    if app.status == 'Line' or app.status == 'Polygon' or app.status == 'Oval':
+        if len(app.objects) > 0:
+            app.objects[-1].assignPoints(event.x, event.y)
+    elif app.status == 'Crop':
+        if app.image.action["crop"]["ing"]:
+            app.image.action["crop"]["ePos"] = (event.x, event.y)
+            app.image.action["crop"]["done"] = True
+
 
 def undo(app):
     if app.objects == []:
@@ -44,8 +72,14 @@ def undo(app):
     app.objects.pop()
 
 
+def timerFired(app):
+    app.image.update()
+
 def redrawAll(app, canvas):
+    # app.image.draw(app, canvas)
     for object in app.objects:
-        object.draw(canvas)
+        object.draw(app, canvas)
+    
+    
 
 runApp(width=500,height=500)
