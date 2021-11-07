c324eb397a44a0d33b017d631efade3ecd06bcb7
Ben Nguyen <hnguyent@andrew.cmu.edu>
2021-11-07 01:41:46 -0400

    Create polygon_buttons.py

---
+++ polygon_buttons.py
@@ -0,0 +1,22 @@
+from button import *
+
+def makePolygonButtons(app):
+    buttons = []
+    polygonButtonActions={'thickness':getPolygonThickness, 'color':getPolygonFill, 'outline':getPolygonOutline}
+
+    y = 50
+    x = 100
+    for label in polygonButtonActions:
+        buttons.append(Button((50,30), location=(x, y), 
+        label=label, fill='light grey', action=polygonButtonActions[label]))
+        x += 60
+    return buttons
+
+def getPolygonThickness(app):
+    app.polygonThickness = int(app.getUserInput('input your polygon thickness here'))
+
+def getPolygonFill(app):
+    app.polygonFill = app.userInput('input your polygon color here')
+
+def getPolygonOutline(app):
+    app.polygonOutline = app.userInput('input your polygon outline color here')
