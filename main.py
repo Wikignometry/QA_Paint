a110f786ed7649f09a4952d99932ff6ddc1bba09
Ben Nguyen <hnguyent@andrew.cmu.edu>
2021-11-06 23:03:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ from oval import *
 from image import *
 
 def appStarted(app):
-    app.status = 'Crop'
+    app.status = 'Line'
     app.image = AppImage()
     app.objects = []
+    app.currentObject = None
 
 def keyPressed(app, event):
     if event.key == 'l':
@@ -19,6 +20,8 @@ def keyPressed(app, event):
         app.status = 'Oval'
     elif event.key == 'c':
         app.status = 'Crop'
+    elif event.key == 'd':
+        app.status = 'Drag'
             
     # https://pythonspot.com/tk-file-dialogs/
     if event.key == "control-o":
@@ -39,27 +42,43 @@ def keyPressed(app, event):
 def mousePressed(app, event):
     if app.status == 'Line':
         app.objects.append(Line(event.x, event.y))
+    
     elif app.status == 'Polygon':
         app.objects.append(Polygon(event.x, event.y))
+    
     elif app.status == 'Oval':
         app.objects.append(Oval(event.x, event.y))
+    
     elif app.status == 'Crop':
         if app.image.action["crop"]["ing"]:
             app.image.action["crop"]["sPos"] = (event.x, event.y)
-
+    
+    elif app.status == 'Drag':
+        for object in app.objects:
+            x0, y0 = object.points[0]
+            x1, y1 = object.points[1]
+            minX, maxX = min(x0, x1), max(x0, x1)
+            minY, maxY = min(y0, y1), max(y0, y1)
+            if minX < event.x < maxX and minY < event.y < maxY:
+                app.currentObject = object
+                object.moveHelper(event.x, event.y)
+                
 def mouseDragged(app, event):
     if app.status == 'Line' or app.status == 'Polygon' or app.status == 'Oval':
         app.objects[-1].currentLocation = (event.x, event.y)
+    
     elif app.status == 'Crop':
         if app.image.action["crop"]["ing"]:
-            app.image.action["crop"]["dPos"] = (event.x, event.y)       
-
-
-
+            app.image.action["crop"]["dPos"] = (event.x, event.y) 
+    
+    elif app.status == 'Drag':
+        app.currentObject.move(event.x, event.y)
+    
 def mouseReleased(app, event):
     if app.status == 'Line' or app.status == 'Polygon' or app.status == 'Oval':
         if len(app.objects) > 0:
             app.objects[-1].assignPoints(event.x, event.y)
+    
     elif app.status == 'Crop':
         if app.image.action["crop"]["ing"]:
             app.image.action["crop"]["ePos"] = (event.x, event.y)
@@ -81,5 +100,5 @@ def redrawAll(app, canvas):
         object.draw(app, canvas)
     
     
-
-runApp(width=500,height=500)
+if __name__ == '__main__':
+    runApp(width=500,height=500)
